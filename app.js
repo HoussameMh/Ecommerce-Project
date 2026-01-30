12e92d46f9fd0d28985892ad793e76aba3556f6d
HoussameMh <mehrachhoussam111@gmail.com>
2026-01-30 22:54:09 +0100

    Install npm packages and Assure Database Connectivity

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+
+
+const port=3000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
